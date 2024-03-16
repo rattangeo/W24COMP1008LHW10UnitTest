1347269a6b0d7182da16c90f0e81e91dadd4a348
Rattan Ashat <rashat@lakeheadu.ca>
2024-03-16 00:15:14 -0400

    Junit Test

--- src/main/java/com/example/w24comp1008lhw10unittest/Rectangle.java
+++ src/main/java/com/example/w24comp1008lhw10unittest/Rectangle.java
@@ -1,2 +1,49 @@
-package com.example.w24comp1008lhw10unittest;public class Rectangle {
+package com.example.w24comp1008lhw10unittest;
+
+public class Rectangle {
+
+    private double height, width;
+
+    public Rectangle(double height, double width) {
+        setHeight(height);
+        setWidth(width);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+
+        if (height>0)
+            this.height = height;
+        else
+            throw new IllegalArgumentException("Height must be greater than 0");
+
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width>0)
+            this.width = width;
+        else
+            throw new IllegalArgumentException("Width must be greater than 0");
+    }
+
+
+    public boolean isSquare(){
+        return width ==height;
+    }
+
+    public double getArea(){
+        return height*width;
+    }
+
+    public double getPerimeter(){
+        return 2*width + 2*height;
+    }
+
 }
--- src/main/tests/com/example/w24comp1008lhw10unittest/RectangleTest.java
+++ src/main/tests/com/example/w24comp1008lhw10unittest/RectangleTest.java
@@ -1,29 +1,113 @@
 package com.example.w24comp1008lhw10unittest;
 
+import org.junit.jupiter.api.Assertions;
+
 import static org.junit.Assert.*;
+
 public class RectangleTest {
+
+    private Rectangle rectangle;
+    private Rectangle square;
+
 
     @org.junit.Before
     public void setUp() throws Exception {
+        rectangle = new Rectangle(20,30);
+        square = new Rectangle(20,20);
+
     }
 
     @org.junit.Test
     public void setHeight() {
+        rectangle.setHeight(50);
+        assertEquals(50,rectangle.getHeight(),0.0);
+
     }
+
+    @org.junit.Test
+    public void setHeightZero() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            rectangle.setHeight(0);
+        });
+    }
+    @org.junit.Test
+    public void setHeightNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            rectangle.setHeight(-1.0);
+        });
+    }
+
+
+
 
     @org.junit.Test
     public void setWidth() {
+        rectangle.setWidth(100);
+        assertEquals(100,rectangle.getWidth(),0.0);
+
     }
 
     @org.junit.Test
+    public void setWidthZero() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            rectangle.setWidth(0);
+        });
+    }
+    @org.junit.Test
+    public void setWidthNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            rectangle.setWidth(-1.0);
+        });
+    }
+
+
+    @org.junit.Test
     public void isSquare() {
+        assertTrue(square.isSquare());
+    }
+    @org.junit.Test
+    public void isSquareFalse() {
+        assertTrue(rectangle.isSquare());
     }
 
     @org.junit.Test
     public void getArea() {
+        //rectangle is 20*30, so the area should be 20*30 =600
+        assertEquals(600,rectangle.getArea(),0.0);
+
     }
 
     @org.junit.Test
     public void getPerimeter() {
+
+        //rectangle that is 20*20, the perimeter should be 20+30+20+30=100
+        assertEquals(100,rectangle.getPerimeter(),0.0);
     }
+
+    @org.junit.Test
+    public void getPerimeterSquare() {
+
+        //rectangle that is 20*20, the perimeter should be 20+20+20+20=80
+        assertEquals(80,square.getPerimeter(),0.0);
+    }
+
+    @org.junit.Test
+    public void setWidthZeroConstructor() {
+
+       Assertions.assertThrows(IllegalArgumentException.class, ()->{
+           new Rectangle(100,0);
+       });
+    }
+    @org.junit.Test
+    public void setHeightZeroConstructor() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            new Rectangle(0,10);
+        });
+    }
+
+
+
+
+
 }
